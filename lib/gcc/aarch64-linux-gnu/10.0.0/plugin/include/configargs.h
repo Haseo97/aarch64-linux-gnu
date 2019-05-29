ac99db77d0be7d49eb1f778ec75d9aad66b3c9b5
Yousef <yusufgadrie@gmail.com>
2019-05-30 05:47:54 +0700

    GCC 10.0.0
    Signed-off-by: Yousef <yusufgadrie@gmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/10.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror CFLAGS='-O2 -fstack-protector-strong' CXXFLAGS='-O2 -fstack-protector-strong' --enable-languages=c --enable- graphite=yes --target=aarch64-linux-gnu --prefix=/root/gcc/aarch64-linux-gnu";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
